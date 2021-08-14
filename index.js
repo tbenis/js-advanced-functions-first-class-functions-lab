2f725bbf9029f8211d0d9ab8e4495afbd2c1fb4b
Benis Tambe <will.jenifer@yahoo.com>
2021-08-14 16:58:12 -0400

    commit tree 23bcc9f2cab68f8f5b9d94197c34bfd1cbca6a7c

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers =  function (arr){
+  return arr.slice(0,2);
+}
+
+const returnLastTwoDrivers =  function (arr){
+  return arr.slice(-2, arr.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = function (multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+  return driversToReturn(drivers);
+};
